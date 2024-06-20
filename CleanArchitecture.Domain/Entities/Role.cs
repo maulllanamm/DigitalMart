58b2415ae71f5f3ae3816ddea210076c27dc4403
Maulana Muhammad <maulllanamuhammad@gmail.com>
2024-06-20 14:04:43 +0700

    feat : add_role_table migration

---
+++ CleanArchitecture.Domain/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
--- CleanArchitecture.Persistence/Context/DataContext.cs
+++ CleanArchitecture.Persistence/Context/DataContext.cs
@@ -9,28 +9,34 @@ namespace CleanArchitecture.Persistence.Context
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Konfigurasi nama tabel
-            modelBuilder.Entity<User>()
-                .ToTable("users");
+            modelBuilder.Entity<User>().ToTable("users");
+            modelBuilder.Entity<Role>().ToTable("roles");
 
-            modelBuilder.Entity<User>()
-                .Property(e => e.created_date)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP"); 
+            // Daftar entitas yang ingin dikonfigurasi
+            var entities = new[] { typeof(User)};
+            foreach (var entity in entities)
+            {
+                modelBuilder.Entity(entity)
+                    .Property("created_date")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
-            modelBuilder.Entity<User>()
-                .Property(e => e.created_by)
-                .HasDefaultValue("system"); 
+                modelBuilder.Entity(entity)
+                    .Property("created_by")
+                    .HasDefaultValue("system");
 
-            modelBuilder.Entity<User>()
-                .Property(e => e.modified_date)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+                modelBuilder.Entity(entity)
+                    .Property("modified_date")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
-            modelBuilder.Entity<User>()
-                .Property(e => e.modified_by)
-                .HasDefaultValue("system");
+                modelBuilder.Entity(entity)
+                    .Property("modified_by")
+                    .HasDefaultValue("system");
+            }
 
 
             base.OnModelCreating(modelBuilder);
